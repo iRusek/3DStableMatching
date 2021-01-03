73f94d36751c084ac0bd36c6ace344a2cede45be
Ido Rusek <idorusek@gmail.com>
2021-01-03 21:22:25 +0200

    Uploaded examples

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-## [![Ido Rusek's header](https://https://github.com/iRusek/3DStableMatch/blob/master/images/Explanation.PNG)]
-
 ### What is this project about?
-In This project I implemented an algorithm to generate large amount of 3 dimensional pursuing (Men, Women and Dogs) in order to understand the power of beign a pursuer rather than being pursued.
+In This project I implemented an algorithm to generate large amount of 3 dimensional matches (Men, Women and Dogs) in order to understand the power of being a pursuer rather than being pursued.
 This challenging idea was shared to me by a good friend - Dor Ben Yehuda, regarding his research on the subject.
 
 ### What is Stable Matching?
@@ -10,9 +8,7 @@ Having a given set of matches between A's and B's, a specific match will be call
 * An A prefers to be with a different B than the one he is already matched with
 &
 * The same B prefers our given A more than the one he is matched with currently.
+## ![Ido Rusek's header](https://github.com/iRusek/3DStableMatching/blob/master/images/explanation.png)
 
 ### 3 Dimensions???
-The idea behind adding a third dimension comes from understanding that opening the matches to have Multi-Participant-Groups opens a large variety of applications for this model, e.g. Surgery Rooms, Supplier-Consumer-Investor.
-
-<a href="https://www.linkedin.com/in/ido-rusek/"><img height="30" src="https://https://github.com/iRusek/3DStableMatch/blob/master/images/Linkedin.png?raw=true"></a>
-</p>
+The idea behind adding a third dimension comes from understanding that Multi-Participant-Groups opens a large variety of applications for this model, e.g. Surgery Rooms, Supplier-Consumer-Investor.
--- generate_data.py
+++ generate_data.py
@@ -1,10 +1,10 @@
 from openpyxl import load_workbook
 import pandas as pd
 import stable_match
-
+import sys
 
 def next_settings(settings):
-    for i in range(4):
+    for i in range(group_size):
         if settings[-i - 1] == 0:
             settings[-i - 1] += 1
             return settings
@@ -20,20 +20,20 @@ def next_settings(settings):
 def create_DB():
     settings = [0, 0, 0, 0, 0]
     df_db = pd.DataFrame()
-    for i in range(100):
+    for i in range(sample_size):
         df_db = df_db.append([settings + stable_match.stable_match(settings)], ignore_index=True)
     while (next_settings(settings)) != "Complete":
-        for i in range(100):
+        for i in range(sample_size):
             df_db = df_db.append([settings + stable_match.stable_match(settings)], ignore_index=True)
     df_db = df_db.rename(columns={0: "Pursuer", 1: "Pursues left?", 2: "Couple are pursued?",
                                   3: "Influencer / Dominant was already the Pursued?", 4: "Fair pursue?",
                                   5: "Men's dissatisfaction", 6: "Women's dissatisfaction", 7: "Dog's dissatisfaction",
                                   8: "# of iterations"})
-    df_db.to_excel("Database.xlsx", sheet_name="Database")
+    df_db.to_excel("examples\Database_"+str(sample_size)+"_"+str(group_size)+".xlsx", sheet_name="Database")
 
 
 def calculate_means():
-    df_db = pd.read_excel("Database.xlsx")
+    df_db = pd.read_excel("examples\Database_"+str(sample_size)+"_"+str(group_size)+".xlsx")
     df_means = df_db.groupby(
         ["Pursuer", "Pursues left?", "Couple are pursued?", "Influencer / Dominant was already the Pursued?",
          "Fair pursue?"]).mean()
@@ -41,13 +41,15 @@ def calculate_means():
         ["Pursuer", "Pursues left?", "Couple are pursued?", "Influencer / Dominant was already the Pursued?",
          "Fair pursue?"]).std()
 
-    writer = pd.ExcelWriter(r"C:\Users\Rusek\1_Python_Projects\3DStableMatching\Means&Std.xlsx", engine='openpyxl')
-    df_means.to_excel("Means&Std.xlsx", sheet_name='Means')
-    writer.book = load_workbook(r"C:\Users\Rusek\1_Python_Projects\3DStableMatching\Means&Std.xlsx")
+    writer = pd.ExcelWriter(r"examples\Means&Std_"+str(sample_size)+"_"+str(group_size)+".xlsx", engine='openpyxl')
+    df_means.to_excel("examples\Means&Std_"+str(sample_size)+"_"+str(group_size)+".xlsx", sheet_name='Means')
+    writer.book = load_workbook(r"examples\Means&Std_"+str(sample_size)+"_"+str(group_size)+".xlsx")
     df_standard_deviation.to_excel(writer, sheet_name='Std', index=False)
     writer.save()
     writer.close()
 
 
+sample_size=int(sys.argv[1])
+group_size=int(sys.argv[2])
 create_DB()
 calculate_means()
